7a55f44a687188c8e320d496adaa55da0c5a58aa
Glizzus <94141567+Glizzus@users.noreply.github.com>
2022-02-13 09:23:53 -0600

    Update about_controller.ex

--- lib/repertoire2_web/controllers/about_controller.ex
+++ lib/repertoire2_web/controllers/about_controller.ex
@@ -1,7 +1,7 @@
 defmodule Repertoire2Web.AboutController do
   use Repertoire2Web, :controller
 
-  def create(conn, _params) do
+  def create_about(conn, _params) do
     render(conn, "about.html")
   end
 end
